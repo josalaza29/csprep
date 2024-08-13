e104719d73d795ab9d668b222161382f9c7ad69c
josalaza29 <jorgesalt29@gmail.com>
2024-08-12 21:06:37 -0700

    C# init

---
+++ cs/MainTesting/datastructures/CSStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainTesting.datastructures
+{
+    internal class CSStack
+    {
+        private int track;
+        private int[] structure;
+        public CSStack() {
+            this.track = 0;
+            this.structure = new int[1];
+        }
+        public CSStack(int capacity) {
+            this.track = 0;
+            this.structure = new int[capacity];
+        }
+
+        public bool push(int value)
+        {
+            if (this.track < this.structure.Length)
+            {
+                this.structure[this.track++] = value;
+                return true;
+            }
+            return false;
+        }
+
+        public int pop()
+        {
+            if (this.track >= 0)
+            {
+                int returnValue = this.structure[this.track];
+                this.structure[--this.track] = 0;
+            }
+            return -1;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("At index: " + this.track + " with capacity: " + this.structure.Length);
+            string result = "";
+            foreach (int i in this.structure)
+            {
+                result += i.ToString() + ", ";
+            }
+            Console.WriteLine("[" + result + "]");
+        }
+
+    }
+}
---
+++ cs/MainTesting/datastructures/DataStructuresMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainTesting.datastructures
+{
+    internal class DataStructuresMain
+    {
+        static void Mainx(string[] args)
+        {
+            //Console.WriteLine("Hello");
+        CSStack stack = new CSStack(7);
+        stack.push(15);
+        stack.print();
+        stack.push(6);
+        stack.push(2);
+        stack.push(9);
+        stack.print();
+        stack.pop();
+        stack.print();
+//        JQueueGeneric que = new JQueueStatic(7);
+            //        que.in(15);
+            //        que.in(6);
+            //        que.in(4);
+            //        que.in(3);
+            //        que.in(8);
+            //        que.in(9);
+            //        que.in(10);
+            //        que.print();
+            //        que.out();
+            //        que.print();
+            //        que.out();
+            //        que.print();
+            //        que.in(1);
+            //        que.out();
+            //        que.print();
+            //        JLinkedList list = new JLinkedList();
+            //        list.lInsert(5);
+            //        list.print();
+            //        list.lInsert(15);
+            //        list.print();
+            //        list.lInsert(25);
+            //        list.print();
+            //        list.lInsert(45);
+            //        list.print();
+            //        list.lDelete(25);
+            //        list.print();
+            //        list.lInsert(25);
+            //        list.print();
+        }
+    }
+}
